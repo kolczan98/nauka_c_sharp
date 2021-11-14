32e3b35035c971d7ec6b813880d82da098d93487
kolczan98 <94317151+kolczan98@users.noreply.github.com>
2021-11-14 20:15:48 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System; //dyrektywa
+
+namespace Lekcja_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(DateTime.Now);
+        }
+    }
+}
